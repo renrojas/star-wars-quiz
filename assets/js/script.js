70cff4857af2fa64ce905d569363a73ebbde8acc
renrojas <karensadithrojas@gmail.com>
2023-05-11 21:57:52 -0600

    added quiz questions, options, and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,61 @@
+const questions = [
+    {
+        questionText: "What was Order 66?",
+        options: [
+            "A. Operation performed on Anakin Skywalker pre Darth Vader",
+            "B. Order that launched the Death Star",
+            "C. Order sent by Leia to find Luke Skywalker", 
+            "D. Order identifying all Jedi as traitors to the Republic thus to summary execution"],
+        answer: "D. Order identifying all Jedi as traitors to the Republic thus to summary execution",
+    },
+    {
+        questionText: "In a New Hope, Leia is kept on the Death Star in what cell number?",
+        options: [
+            "A. Cell 54",
+            "B. Cell 5237",
+            "C. Cell 2187",
+            "D. Cell IV"],
+        answer: "C. Cell 2187",
+    },
+    {
+        questionText: "What species is Jar Jar Binks?",
+        options: [
+            "A. Abyssin", 
+            "B. Sarlacc",
+            "C. Gungan",
+            "D. Chiss"],
+        answer: "C. Gungan", 
+    },
+    {
+        questionText: "Theed is the capital of what planet?",
+        options: [    
+            "A. Naboo",
+            "B. Coruscant", 
+            "C. Tatooine", 
+            "D.  Felucia"],
+        answer: "A. Naboo",
+    },
+    {
+        questionText: "What is the name of the Wookiees' home planet?",
+        options: [
+            "A. Kamino", 
+            "B. Kashyyyk",
+            "C. Mustafar",
+            "D. Yavin"],
+        answer: "B. Kashyyyk",
+    },
+    {
+        questionText: "What creates the color of a lightsaber?",
+        options: [
+            "A. Gas",
+            "B. Light filaments",
+            "C. Force of the one holding it",
+            "D. Kyber crystals"],
+        answer: "D. Kyber crystals",
+    },
+];
+
+
 var startButton = document.getElementById("start-button");
 
 function startQuiz (){
@@ -16,6 +74,7 @@ function timerStart() {
  var timeEl= document.getElementById("time")
  timeEl.textContent = "time: " + counter;
  counter=counter - 1
+
 }
 
 
